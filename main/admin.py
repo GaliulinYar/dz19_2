bb0baeaae47d0350c3cd31acc83a44f6298b0685
Yaroslav Galiulin <yaroslav66@list.ru>
2023-08-03 11:38:25 +0500

    Внесение правок

--- main/admin.py
+++ main/admin.py
@@ -9,12 +9,12 @@ from main.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = (id, 'name_prod', 'price_prod', 'category_prod')
+    list_display = ('id', 'name_prod', 'price_prod', 'category_prod')
     list_filter = ('category_prod',)
     search_fields = ('name_prod', 'description_prod')
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = (id, 'name_category')
+    list_display = ('id', 'name_category')
 
