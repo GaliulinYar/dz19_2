d258fafcee0855bebefdf023f56196482b8b38c4
Yaroslav Galiulin <yaroslav66@list.ru>
2023-08-05 17:37:06 +0500

    Создание связи в моделях для БД

--- main/models.py
+++ main/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     description_prod = models.CharField(max_length=100, verbose_name='Описание')
     img_prod = models.ImageField(upload_to='preview', verbose_name='Превью', null=True, blank=True)
     category_prod = models.CharField(max_length=100, verbose_name='Категория')
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, verbose_name='Категория', null=True, blank=True)
     price_prod = models.IntegerField(verbose_name='Цена', null=True, blank=True, help_text='Введите цену продукта в рублях')
     data_create_prod = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
     data_change_prod = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
