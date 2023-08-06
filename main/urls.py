94848871397cbca265a460ed1ce43bd0a5a2f780
Yaroslav Galiulin <yaroslav66@list.ru>
2023-08-06 13:01:03 +0500

    Прописывание урлов новой страницы для добавления новых продуктов

--- main/urls.py
+++ main/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from main.views import index, contacts, product
+from main.views import index, contacts, product, great_prod
 
 urlpatterns = [
     path('', index),
     path('contacts/', contacts),
     path('product/<int:product_id>/', views.product, name='product'),
+    path('great_prod/', great_prod)
 ]
 
 
