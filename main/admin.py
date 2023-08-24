b4bd27594b8c76b3e7bfccc90d29d1ab2a7dae79
Yaroslav Galiulin <yaroslav66@list.ru>
2023-08-24 14:20:45 +0500

    Изменение админки

--- main/admin.py
+++ main/admin.py
@@ -9,8 +9,8 @@ from main.models import Product, Category
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name_prod', 'price_prod', 'category_prod')
-    list_filter = ('category_prod',)
+    list_display = ('id', 'name_prod', 'price_prod', 'category')
+    list_filter = ('category',)
     search_fields = ('name_prod', 'description_prod')
 
 
