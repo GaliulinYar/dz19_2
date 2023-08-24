4471ba1836fad9484218adafba8d1acc49aa529c
Yaroslav Galiulin <yaroslav66@list.ru>
2023-08-24 14:21:48 +0500

    Дополнительные действия

---
+++ main/forms.py
@@ -0,0 +1,33 @@
+from django import forms
+
+from main.models import Product
+
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+        # fields = ('name_prod', 'description_prod', 'img_prod')
+        # exclude = () # поля которые исключаются
+
+    def clean_name_prod(self):
+        cleaned_data = self.cleaned_data['name_prod']
+
+        if cleaned_data in ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                        'радар']:
+            raise forms.ValidationError('Ой, всё, запрещенное слово')
+
+        return cleaned_data
+
+    def clean_description_prod(self):
+        cleaned_data = self.cleaned_data['description_prod']
+
+        if cleaned_data in ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
+                        'радар']:
+            raise forms.ValidationError('Ой, всё, запрещенное слово')
+
+        return cleaned_data
+
+
+
