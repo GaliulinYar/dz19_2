51f7c18b1098a00be7c028f84824afa9cced78a0
Yaroslav Galiulin <yaroslav66@list.ru>
2023-08-29 11:48:25 +0500

    Формы версий

--- main/forms.py
+++ main/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 
-from main.models import Product
+from main.models import Product, Version
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if field_name != "is_current_version":
+                field.widget.attrs['class'] = 'form-control'
 
 
 class ProductForm(forms.ModelForm):
@@ -14,8 +22,8 @@ class ProductForm(forms.ModelForm):
     def clean_name_prod(self):
         cleaned_data = self.cleaned_data['name_prod']
 
-        if cleaned_data in ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
-                        'радар']:
+        if cleaned_data.title() in ['Казино', 'Криптовалюта', 'Крипта', 'Биржа', 'Дешево', 'Бесплатно', 'Обман', 'Полиция',
+                        'Радар']:
             raise forms.ValidationError('Ой, всё, запрещенное слово')
 
         return cleaned_data
@@ -23,11 +31,20 @@ class ProductForm(forms.ModelForm):
     def clean_description_prod(self):
         cleaned_data = self.cleaned_data['description_prod']
 
-        if cleaned_data in ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',
-                        'радар']:
+        if cleaned_data.title() in ['Казино', 'Криптовалюта', 'Крипта', 'Биржа', 'Дешево', 'Бесплатно', 'Обман', 'Полиция',
+                        'Радар']:
             raise forms.ValidationError('Ой, всё, запрещенное слово')
 
         return cleaned_data
+
+
+class VersionForm(forms.ModelForm):
+    class Meta:
+        model = Version
+        fields = '__all__'
+        # fields = ('name_prod', 'description_prod', 'img_prod')
+        # exclude = () # поля которые исключаются
+
 
 
 
